49b4a75b5fe76195d7d132e28494da885867275b
jmarcantony <josephmarc2610@gmail.com>
2021-08-16 19:44:52 +0400

    Created Base Flask App

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Helth")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.get("/")
+def index():
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", debug=True)
