d3991c32ba48edf65c4d86aa7e937b35b84b624d
ninjahacker123 <josephmarc2610@gmail.com>
2021-08-24 18:22:00 +0400

    Succesfully setup firestore

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template
+from firebase_admin import credentials
+from firebase_admin import firestore
 from dotenv import load_dotenv
+import firebase_admin
+import os
 
 load_dotenv()
 
+GOOGLE_APPLICATION_CREDENTIALS = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
 app = Flask(__name__)
+
+cred = credentials.Certificate(GOOGLE_APPLICATION_CREDENTIALS)
+firebase_admin.initialize_app(cred)
+
+db = firestore.AsyncClient()
 
 @app.get("/")
 def index():
